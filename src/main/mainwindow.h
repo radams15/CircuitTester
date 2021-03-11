6cf939c3f422691ec3ff7986ec9b69726d16cf30
Rhys Adams <email.curly@gmail.com>
2021-03-11 16:24:03 +0000

    Added documentation generator (doxygen). Can be easily disabled if I need to (e.g. at school when doxygen is not installed)

--- src/main/mainwindow.h
+++ src/main/mainwindow.h
@@ -3,11 +3,23 @@
 
 #include <QMainWindow>
 
+/** @brief Main window class for the program
+ * @author Rhys Adams
+ * @date 11/03/2021
+ */
+
 class MainWindow : public QMainWindow{
     Q_OBJECT
 
 public:
+    /**
+     * Initialises the main window
+     * @param parent - The parent window, default to nullptr for toplevel
+     */
     MainWindow(QWidget *parent = nullptr);
+    /**
+     * Deletes the window when finished
+     */
     ~MainWindow();
 
 private:
