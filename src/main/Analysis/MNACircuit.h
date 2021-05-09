7b5952276110e4fd0beede02946be7255aa15f0f
Rhys Adams <email.curly@gmail.com>
2021-05-09 20:03:03 +0100

    Added documentation for parts of MNACircuit.

--- src/main/Analysis/MNACircuit.h
+++ src/main/Analysis/MNACircuit.h
@@ -1,6 +1,7 @@
-//
-// Created by rhys on 18/04/2021.
-//
+/** @brief Circuit class for the modified nodal analysis
+ * @author Rhys Adams
+ * @date 18/04/2021
+ */
 
 #ifndef CircuitTester_MNACIRCUIT_H
 #define CircuitTester_MNACIRCUIT_H
@@ -15,31 +16,32 @@
 
 class MNACircuit {
 private:
-
-public:
+    /** @brief Holds all the MNAElements that are batteries */
     std::vector<MNAElement*> batteries;
+
+    /** @brief Holds all the MNAElements that are resistors */
     std::vector<MNAElement*> resistors;
+
+    /** @brief Holds all the MNAElements that are current sources */
     std::vector<MNAElement*> currentSources;
 
+    /** @brief Holds all the MNAElements combined */
     std::vector<MNAElement*> elements;
 
+    /** @brief A map of node names against node values */
     std::map<int, int> nodeSet;
-    int nodeCount;
-    std::vector<int> nodes;
 
-    explicit MNACircuit(std::vector<MNAElement *> elements);
+    /** @brief The total number of nodes */
+    int nodeCount;
+
+    /** @brief All the nodes in the circuit*/
+    std::vector<int> nodes;
 
     int getNumUnknownCurrents();
 
     int getNumVars();
 
-    double getCurrentTotal(int nodeIndex);
-
-    std::vector<Term*>* getCurrents(int node, int side, int sign);
-
     std::vector<int>* getRefNodes();
-
-    std::vector<int>* getConnectedNodes(int node);
 
     std::vector<Equation*>* getEquations();
 
@@ -47,8 +49,27 @@ public:
 
     int getElementIndex(std::vector<Unknown*>* array, Unknown* element);
 
-    MNASolution* solve();
-};
+public:
+    /** @brief Initialises MNACircuit object.
+     * This initialises the MNACircuit, as well as categorising
+     * the elements into the batteries, resistors and currentSources
+     * vectors. Also generates nodeSet, nodeCount and nodes variables.
+     *
+     * @param elements The list of all circuit elements.
+    */
+    explicit MNACircuit(std::vector<MNAElement *> elements);
 
+    /** @brief Solves the circuit using linear algebra and matrices.
+     *
+     * @return An MNASolution class for this circuit.
+     */
+    MNASolution* solve();
+
+    double getCurrentTotal(int nodeIndex);
+
+    std::vector<int>* getConnectedNodes(int node);
+
+    std::vector<Term*>* getCurrents(int node, int side, int sign);
+};
 
 #endif //CircuitTester_MNACIRCUIT_H
